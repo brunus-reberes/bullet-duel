c36ddd2c800538a7e6d2e407a613319e9f39c32e
Bruno Ribeiro <1180573@isep.ipp.pt>
2021-09-01 22:37:10 +0100

    Added Menu - parcial

--- bulletduel/main.py
+++ bulletduel/main.py
@@ -1,10 +1,14 @@
 import curses
 from domain import * 
+from menus import *
 import config
+
 
 def main(stdscr):
 
     Window(config.window_width, config.window_heigth).print(stdscr)
+
+    MainMenu().print(stdscr)
 
     
     stdscr.getkey()
