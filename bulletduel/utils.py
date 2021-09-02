98af7e565f3bd024c8c657130dff0a93e4947862
Bruno Ribeiro <1180573@isep.ipp.pt>
2021-09-02 04:05:20 +0100

    created utils

---
+++ bulletduel/utils.py
@@ -0,0 +1,28 @@
+import math
+
+def draw(stdscr, sprite, y, x):
+    i = y
+    for line in sprite:
+        stdscr.addstr(i, x, line)
+        i += 1
+
+def drawr(stdscr, sprite, y, x):
+    draw(stdscr, sprite, y, x)
+    stdscr.refresh()
+
+
+def compensate(sprite):
+    lens = [len(line) for line in sprite]
+    highest_len = max(lens)
+    return [_compensate(line, highest_len) for line in sprite]
+
+def _compensate(line, highest_len):
+    spaces = highest_len - len(line)
+    half_spaces = round(spaces / 2)
+    empty = ' ' * half_spaces
+    line = empty + line
+    return line
+    
+
+
+
