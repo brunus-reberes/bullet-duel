bdcc42bbd0644d8df9b19e443c266287d55fb054
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-15 16:33:53 +0100

    Fix draw function of Sprite blueprint

--- bulletduel/resources/blueprint.py
+++ bulletduel/resources/blueprint.py
@@ -11,37 +11,31 @@ class Sprite:
             self.blank.append(len(line) * " ")
         self.y = None
         self.x = None
-
-    @property
-    def height(self):
-        return len(self.image)
-
-    @property
-    def width(self):
-        return max([len(line) for line in self.image])
+        self.height = len(self.image)
+        self.width = max([len(line) for line in self.image])    
 
     def draw(self, stdscr: curses.window, y: int = None, x: int = None):
         self.y = self.y if y is None else y
         self.x = self.x if x is None else x
-        self._print(stdscr)
+        for y, line in enumerate(self.image, self.y):
+            try:
+                stdscr.addstr(y, self.x, line)
+            except Exception as e:
+                import logging
+                logger = logging.getLogger(__name__)
+                logger.exception(e)
 
     def move(self, stdscr, width, height):
         self.erase(stdscr)
         self.y += height
         self.x += width
-        self._print(stdscr)
+        self.draw(stdscr)
 
     def erase(self, stdscr: curses.window):
         if not self.y is None and not self.x is None:
             for y, line in enumerate(self.blank, self.y):
                 stdscr.addstr(y, self.x, line)
-
-    def _print(self, stdscr: curses.window):
-        for y, line in enumerate(self.image, self.y):
-            try:
-                stdscr.addstr(y, self.x, line)
-            except Exception:
-                pass
+        
 
     def __repr__(self) -> str:
         return "\n".join(self.image)
@@ -62,26 +56,26 @@ class WindowFrame:
     def print(self):
         try:
             self.stdscr.clear()
-
             # TOP_BOTTOM
             for x in range(1, self.width - 1):
                 self.stdscr.addch(0, x, self.top_bottom)  # TOP
                 self.stdscr.addch(self.height - 1, x, self.top_bottom)
-
             # LEFT_RIGHT
             for y in range(1, self.height - 1):
                 self.stdscr.addch(y, 0, self.left_right)
                 self.stdscr.addch(y, self.width - 1, self.left_right)
-
             # CORNERS
             self.stdscr.addch(0, 0, self.corners)
             self.stdscr.addch(0, self.width - 1, self.corners)
             self.stdscr.addch(self.height - 1, 0, self.corners)
             self.stdscr.addch(self.height - 1, self.width - 1, self.corners)
-
+            # Print frame
             self.stdscr.refresh()
-        except Exception:
-            raise Exception("Resize Window")
+        except Exception as e:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.info("Resize terminal window")
+            logger.exception(e)
 
     def child_window(self):
         return curses.newwin(self.height - 2, self.width - 2, 1, 1)
@@ -106,12 +100,6 @@ class Window:
         pass
 
 
-class Menu(Sprite):
-    def __init__(self, options: list[str]) -> None:
-        self.options = options
-        highest_len = max([len(line) for line in options])
-        new_options = []
-        for line in options:
-            spaces = " " * round((highest_len - len(line)) / 2)
-            new_options.append(spaces + line)
-        super().__init__(new_options)
+class Selector:
+    def __init__(self, options: list[str], menus: list[Window]) -> None:
+        pass
