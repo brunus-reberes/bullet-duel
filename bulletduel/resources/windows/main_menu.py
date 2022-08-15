725b58188ec9bb244cbe2e692df62d8aff5a728f
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-15 16:35:32 +0100

    Fix main menu window

--- bulletduel/resources/windows/main_menu.py
+++ bulletduel/resources/windows/main_menu.py
@@ -1,30 +1,29 @@
 import resources.button as button
-from resources.blueprint import Menu, Window
+from resources.blueprint import Window
 from resources.sprites.factory import (logo, pointer_left,
-                                                  pointer_right)
+                                                  pointer_right, menu)
 
 
 class MainMenu(Window):
     def setup(self) -> None:
         self.logo = logo()
         self.logo.x = self.win_width_mid - int(self.logo.width / 2)
-        self.logo.y = 3
+        self.logo.y = 2
 
-        self.menu = Menu(["Duel", "Campaign", "Rankings", "Settings", "Exit"])
+        self.options = ["Duel", "Campaign", "Rankings", "Settings", "Exit"]
+        self.menu = menu(self.options)
         self.menu.x = self.win_width_mid - int(self.menu.width / 2)
-        self.menu.y = 15
+        self.menu.y = 14
 
         self.left_pointer = pointer_right()
         self.left_pointer.x = self.menu.x - 3 - self.left_pointer.width
-        self.left_pointer.y = 14
-
+        self.left_pointer.y = 13
+        
         self.right_pointer = pointer_left()
         self.right_pointer.x = self.menu.x + self.menu.width + 3 
-        self.right_pointer.y = 14
+        self.right_pointer.y = 13
 
     def run(self):
-        y_min = 14
-        y_max = 17
         self.logo.draw(self.stdscr)
         self.menu.draw(self.stdscr)
         self.left_pointer.draw(self.stdscr)
@@ -33,10 +32,10 @@ class MainMenu(Window):
             c = self.stdscr.getch()
             if c == ord("q"):
                 break
-            elif c == button.KEY_UP and self.left_pointer.y > y_min:
+            elif c == button.KEY_UP and self.left_pointer.y > self.menu.y-1:
                 self.left_pointer.move(self.stdscr, 0, -1)
                 self.right_pointer.move(self.stdscr, 0, -1)
-            elif c == button.KEY_DOWN and self.left_pointer.y < y_max:
+            elif c == button.KEY_DOWN and self.left_pointer.y < self.menu.y + self.menu.height - 2:
                 self.left_pointer.move(self.stdscr, 0, +1)
                 self.right_pointer.move(self.stdscr, 0, +1)
             elif c == button.KEY_ENTER:
