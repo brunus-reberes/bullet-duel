467114ede02f859c5bb17218287df43b4bb49f87
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-15 16:32:31 +0100

    Fix logging system

--- bulletduel/start.py
+++ bulletduel/start.py
@@ -7,21 +7,19 @@ from resources.windows.main_menu import MainMenu
 
 
 def main(stdscr: curses.window):
-    curses.curs_set(0)
-    window = WindowFrame(stdscr, window_width, window_height).child_window()
-    MainMenu(window)
-
-
-if __name__ == "__main__":
+    logging.basicConfig(filename="system.log", filemode="w")
     logger = logging.getLogger(__name__)
-    fh = logging.FileHandler("system.log")
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    logger.addHandler(fh)
-    logger.addHandler(ch)
     try:
         logger.info("Starting game")
-        curses.wrapper(main)
+        
+        curses.curs_set(0)
+        window = WindowFrame(stdscr, window_width, window_height).child_window()
+        MainMenu(window)
+
         logger.info("Exiting game")
     except Exception as err:
         logger.exception(err)
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
