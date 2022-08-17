3ace10d7cd14ddfbcdc833afc4f6d7791d3ec122
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-17 21:46:40 +0100

    Add Menu to the main menu

--- bulletduel/resources/windows/main_menu.py
+++ bulletduel/resources/windows/main_menu.py
@@ -1,8 +1,9 @@
 import resources.button as button
-from resources.blueprint import Window
-from resources.sprites.factory import (logo, pointer_left,
-                                                  pointer_right, menu)
-
+from resources.blueprint import Window, Menu
+from resources.sprites.factory import logo, pointer_left, pointer_right
+from resources.windows.in_development import InDevelopment
+import logging
+logger = logging.getLogger(__name__)
 
 class MainMenu(Window):
     def setup(self) -> None:
@@ -10,33 +11,41 @@ class MainMenu(Window):
         self.logo.x = self.win_width_mid - int(self.logo.width / 2)
         self.logo.y = 2
 
-        self.options = ["Duel", "Campaign", "Rankings", "Settings", "Exit"]
-        self.menu = menu(self.options)
-        self.menu.x = self.win_width_mid - int(self.menu.width / 2)
-        self.menu.y = 14
+        self.options = [
+            ("Duel", InDevelopment(self.stdscr)),
+            ("Campaign", InDevelopment(self.stdscr)),
+            ("Rankings", InDevelopment(self.stdscr)),
+            ("Settings", InDevelopment(self.stdscr)),
+            ("Exit", None),
+        ]
+        self.menu = Menu(self.options)
+        self.menu.sprite.x = self.win_width_mid - int(self.menu.sprite.width / 2)
+        self.menu.sprite.y = 14
 
         self.left_pointer = pointer_right()
-        self.left_pointer.x = self.menu.x - 3 - self.left_pointer.width
+        self.left_pointer.x = self.menu.sprite.x - 3 - self.left_pointer.width
         self.left_pointer.y = 13
-        
+
         self.right_pointer = pointer_left()
-        self.right_pointer.x = self.menu.x + self.menu.width + 3 
+        self.right_pointer.x = self.menu.sprite.x + self.menu.sprite.width + 3
         self.right_pointer.y = 13
 
     def run(self):
+        self.stdscr.clear()
         self.logo.draw(self.stdscr)
-        self.menu.draw(self.stdscr)
+        self.menu.sprite.draw(self.stdscr)
         self.left_pointer.draw(self.stdscr)
         self.right_pointer.draw(self.stdscr)
-        while True:
+        exit = False
+        while not exit:
             c = self.stdscr.getch()
             if c == ord("q"):
                 break
-            elif c == button.KEY_UP and self.left_pointer.y > self.menu.y-1:
+            elif c == button.KEY_UP and self.menu.previous():
                 self.left_pointer.move(self.stdscr, 0, -1)
                 self.right_pointer.move(self.stdscr, 0, -1)
-            elif c == button.KEY_DOWN and self.left_pointer.y < self.menu.y + self.menu.height - 2:
+            elif c == button.KEY_DOWN and self.menu.next():
                 self.left_pointer.move(self.stdscr, 0, +1)
                 self.right_pointer.move(self.stdscr, 0, +1)
             elif c == button.KEY_ENTER:
-                break
+                exit = self.menu.select().start()
