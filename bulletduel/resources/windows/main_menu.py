637264e897f5df4a888f90dabcdc4621c6960206
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-20 15:12:24 +0100

    Update MainMenu to the newer menu system

--- bulletduel/resources/windows/main_menu.py
+++ bulletduel/resources/windows/main_menu.py
@@ -1,9 +1,12 @@
-import resources.button as button
-from resources.blueprint import Window, Menu
-from resources.sprites.factory import logo, pointer_left, pointer_right
-from resources.windows.in_development import InDevelopment
 import logging
+
+import resources.button as button
+from resources.blueprint import Menu, Window
+from resources.sprites.factory import logo, pointer_left, pointer_right
+from resources.windows import Exit, InDevelopment
+
 logger = logging.getLogger(__name__)
+
 
 class MainMenu(Window):
     def setup(self) -> None:
@@ -12,13 +15,13 @@ class MainMenu(Window):
         self.logo.y = 2
 
         self.options = [
-            ("Duel", InDevelopment(self.stdscr)),
-            ("Campaign", InDevelopment(self.stdscr)),
-            ("Rankings", InDevelopment(self.stdscr)),
-            ("Settings", InDevelopment(self.stdscr)),
-            ("Exit", None),
+            ("Duel", InDevelopment),
+            ("Campaign", InDevelopment),
+            ("Rankings", InDevelopment),
+            ("Settings", InDevelopment),
+            ("Exit", Exit),
         ]
-        self.menu = Menu(self.options)
+        self.menu = Menu(self.stdscr, self.options)
         self.menu.sprite.x = self.win_width_mid - int(self.menu.sprite.width / 2)
         self.menu.sprite.y = 14
 
@@ -30,22 +33,23 @@ class MainMenu(Window):
         self.right_pointer.x = self.menu.sprite.x + self.menu.sprite.width + 3
         self.right_pointer.y = 13
 
-    def run(self):
+        # draw base image
         self.stdscr.clear()
         self.logo.draw(self.stdscr)
         self.menu.sprite.draw(self.stdscr)
         self.left_pointer.draw(self.stdscr)
         self.right_pointer.draw(self.stdscr)
-        exit = False
-        while not exit:
-            c = self.stdscr.getch()
-            if c == ord("q"):
-                break
-            elif c == button.KEY_UP and self.menu.previous():
-                self.left_pointer.move(self.stdscr, 0, -1)
-                self.right_pointer.move(self.stdscr, 0, -1)
-            elif c == button.KEY_DOWN and self.menu.next():
-                self.left_pointer.move(self.stdscr, 0, +1)
-                self.right_pointer.move(self.stdscr, 0, +1)
-            elif c == button.KEY_ENTER:
-                exit = self.menu.select().start()
+
+    def run(self):
+        c = self.stdscr.getch()
+        if c == ord("q"):
+            self.exit = True
+        elif c == button.KEY_UP and self.menu.previous():
+            self.left_pointer.move(self.stdscr, 0, -1)
+            self.right_pointer.move(self.stdscr, 0, -1)
+        elif c == button.KEY_DOWN and self.menu.next():
+            self.left_pointer.move(self.stdscr, 0, +1)
+            self.right_pointer.move(self.stdscr, 0, +1)
+        elif c == button.KEY_ENTER:
+            self.menu.select()
+            self.refresh = True
