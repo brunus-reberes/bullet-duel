712ac4c5b97dce2808edd2aa1eb4206fa69574bb
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-08-20 15:09:27 +0100

    Add exit window

---
+++ bulletduel/resources/windows/exit.py
@@ -0,0 +1,12 @@
+import logging
+from resources.blueprint import Window
+
+logger = logging.getLogger(__name__)
+
+
+class Exit(Window):
+    def setup(self) -> None:
+        self.exit = True
+
+    def run(self):
+        logger.info('exit game')
